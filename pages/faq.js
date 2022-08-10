c2945dad6ba24064e2977fdc8cdb652ac1d49d3b
Gislene Lima <vann.n.a0044@gmail.com>
2022-08-10 00:06:42 -0300

    inserindo arquivos de curso

---
+++ pages/faq.js
@@ -0,0 +1,11 @@
+import NextLink from 'next/link';
+import Link from '../src/components/Link';
+
+export default function FAQPage(){
+    return(
+        <div>
+            <h1>Alura Cases - Página de Perguntas do FAQ</h1>
+            <Link href="/">Ir para a Home</Link>
+        </div>
+    )
+}
